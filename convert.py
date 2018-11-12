75d86e3ea70077e6b28f4658b4f229ef91ea4926
Ryszard Sikora <ryszard.sikora@avanade.com>
2018-11-12 19:16:47 +0100

    final version

--- convert.py
+++ convert.py
@@ -25,6 +25,101 @@ class NetworkXEdge:
     def to_string(self):
         return self.left + ';' + self.right + ';' + str(self.weight)
 
+#From https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance
+def lev(s, t):
+    if s == t: return 0
+    if abs(len(s)-len(t)) > 1: return 10
+    elif len(s) == 0: return len(t)
+    elif len(t) == 0: return len(s)
+    v0 = [None] * (len(t) + 1)
+    v1 = [None] * (len(t) + 1)
+    for i in range(len(v0)):
+        v0[i] = i
+    for i in range(len(s)):
+        v1[0] = i + 1
+        for j in range(len(t)):
+            cost = 0 if s[i] == t[j] else 1
+            v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+        for j in range(len(v0)):
+            v0[j] = v1[j]
+
+    return v1[len(t)]
+
+
+#Polish words from sjp.pl
+words = []
+with open("slowa.txt", encoding='UTF-8') as f:
+    words = f.readlines()
+words = [x.strip() for x in words]
+
+# counter of each word occurence
+words_count = dict()
+# Words that were replaced because they are not in the dictionary
+replacements = dict()
+# Empty answer or string PUSTE
+special_answers = []
+
+expressions = []
+
+rejected = []
+
+#Looks for similar word
+def get_similar_words(word):
+    for w in words:
+        levs = lev(word, w)
+        if levs < 2:
+            if w in words_count:
+                yield (w, words_count[w], levs)
+            else:
+                yield (w, 0, levs)
+
+def replace_single_word(word, counter):
+
+    if(word.strip().lower() == 'puste' or word.strip() == ''):
+        special_answers.append(word)
+        return None
+
+    similar = list(get_similar_words(word))
+    if counter > 2: # Document this
+        return word
+    elif len(similar) == 0:
+        rejected.append(word)
+        return None
+    else:
+        # sort by lev distance then by occurences count
+        similar.sort(key=lambda sim: (sim[1], sim[2]))
+        return similar[0][0]
+
+
+def replace_expression(expr, counter):
+    expressions.append(expr)
+    return expr
+
+
+
+
+
+def add_to_words_count(word, counter, key):
+
+    if key or word in words:
+        if word in words_count:
+            words_count[word] += counter
+        else:
+            words_count[word] = counter
+        return word
+
+    if len(word.split()) > 1:
+        return replace_expression(word, counter)
+
+    else:
+        replaced = replace_single_word(word, counter)
+        if replaced is None:
+            return None
+        if replaced != word:
+            replacements[word] = replaced
+        return replaced
+
+
 with open(outfile, 'a', encoding='utf-8') as output:
     for filename in filenames:
         with open(directory_name + filename, 'r', encoding='utf-8') as file:
@@ -32,8 +127,40 @@ with open(outfile, 'a', encoding='utf-8') as output:
             parsed_list = list(reader)
             for item in parsed_list:
                 edge_name = filename.replace('.csv', '')
-                edges_list.append(NetworkXEdge(edge_name, item[1], item[0]))
+                word = item[1]
+                counter = int(item[0])
+                word = add_to_words_count(word, counter, False)
+                if word is None:
+                    continue
+                edge_name = add_to_words_count(edge_name, counter, True)
+                edges_list.append(NetworkXEdge(edge_name, word, counter))
 
     for edge in edges_list:
         output.write(edge.to_string()+'\n')
+
+with open('report.txt', 'a', encoding='utf-8') as report:
+    for r, v in replacements.items():
+        report.write(f'Replacing {r} with {v}\n')
+    for ex in expressions:
+        report.write(f'Found expression: {ex}\n')
+    for sp in special_answers:
+        report.write(f'Removing special answer {sp}\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- test.py
+++ test.py
@@ -22,10 +22,25 @@ Yv = [pos[k][1] for k in G.nodes()]
 Xed = []
 Yed = []
 weights = []
+weights_extra = []
+X_hidden = []
+Y_hidden = []
+
 for edge in G.edges():
     weights += [str(G.get_edge_data(edge[0], edge[1])['weight'])]
+    weights_extra += [weights[-1] + ' ' + edge[0] + ' ' + edge[1]]
     Xed += [pos[edge[0]][0], pos[edge[1]][0], None]
     Yed += [pos[edge[0]][1], pos[edge[1]][1], None]
+    X_hidden += [(pos[edge[0]][0] + pos[edge[1]][0])/2]
+    Y_hidden += [(pos[edge[0]][1] + pos[edge[1]][1])/2]
+
+max_weight = max(list(map(float, weights)))
+inverted_weights = [(max_weight+1)]*len(weights)
+inverted_weights = [i - float(j) for i, j in zip(inverted_weights, weights)]
+idx = 0
+for edge in G.edges():
+    G.get_edge_data(edge[0], edge[1])['weight'] = inverted_weights[idx]
+    idx += 1
 
 trace3 = Scatter(x=Xed,
                  y=Yed,
@@ -47,8 +62,24 @@ trace4 = Scatter(x=Xv,
                              line=dict(color='rgb(50,50,50)', width=0.5)
                              ),
                  text=G.nodes(),
+
                  hoverinfo='text'
                  )
+
+trace5 = Scatter(
+    x = X_hidden,
+    y = Y_hidden,
+    mode='text',
+    marker=dict(symbol='circle-dot',
+                size=5,
+                color='#6959CD',
+                line=dict(color='rgb(50,50,50)', width=0.5)
+                ),
+    text=weights,
+    hovertext=weights_extra,
+    hoverinfo='text',
+    hoveron='points+fills'
+)
 
 annot = "Graf skojarzeń dla słów: chłopiec, dziecko, mały, dziewczyna"
 
@@ -93,7 +124,16 @@ layout=Layout(title= "Graf skojarzeń dla słów: chłopiec, dziecko, mały, dzi
         ]
     )
 
-data1 = [trace3, trace4]
+data1 = [trace3, trace4, trace5]
 fig1 = Figure(data=data1, layout=layout)
 fig1['layout']['annotations'][0]['text'] = annot
 py.iplot(fig1, filename='semantyka1')
+
+all_paths = nx.shortest_path_length(G, weight='weight')
+paths2 = nx.all_pairs_dijkstra_path(G)
+
+with open('weights2.txt', 'a', encoding='utf-8') as report:
+    for src, target in all_paths.items():
+        for second, value in target.items():
+            if value > 0:
+                report.write(f'{src} -> {second} (Distance: {value}, Nodes: {paths2[src][second]})\n')
